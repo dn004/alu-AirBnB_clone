356e1b7d2d034f63315b2b0d55c5fe8556acd3a0
emelyne1234 <e.uwimbabazai@alustudent.com>
2023-02-04 05:55:47 -0800

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -3,34 +3,38 @@
 import uuid
 from datetime import datetime
 
-#defining all common attributes/methods for other classes
-class BaseModel():
+""" defining all common attributes/methods for other classes """
+
+
+class BaseModel:
     
-    #initialize class
+    """initialize class"""
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, v in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                   value = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                   setattr(self, key, value)
-                   continue
+                    value = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                    continue
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
     def __str__(self):
-       #represent as string
+        """ represent
+        as string"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     
     def save(self):
-        # update to current time
+        """update to current time"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        #return dictionary with all values
+        """return dictionary
+        with all values """
         the_dict = self.__dict__.copy()
         the_dict['__class__'] = self.__class__.__name__
         the_dict['self.created_at'] = self.created_at.isoformat()
-        the_dict['self.created_at'] = self.updated_at.isoformat()
+        the_dict['self.updated_at'] = self.updated_at.isoformat()
         return the_dict
