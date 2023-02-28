39eacbc7d7c20828cb0f99e76802ffcb239d694b
emelyne1234 <105202770+emelyne1234@users.noreply.github.com>
2023-02-28 11:57:59 +0200

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,49 +1,21 @@
 #!/usr/bin/python3
-
-
 import unittest
-import pep8
-from datetime import datetime
-import models
 from models.user import User
-from io import StringIO
-from contextlib import redirect_stdout
 
 
 class TestUser(unittest.TestCase):
     def test_class(self):
-        """creating object and testing class user"""
+        """Tests the class of an instance of User"""
         user = User()
         self.assertEqual(user.__class__.__name__, "User")
-    
-    def setUp(self):
-        self.user = User()
 
-    def test_last_name(self):
-        self.assertIsInstance(self.user.last_name, str)
-        self.assertEqual(self.user.last_name, "")
-    def test_first_name(self):
-        self.assertIsInstance(self.user.first_name, str)
-        self.assertEqual(self.user.first_name, "")
+    def test_user_attributes(self):
+        """Testing the attributes"""
+        user = User()
+        self.assertEqual(user.email, "")
+        self.assertEqual(user.password, "")
+        self.assertEqual(user.first_name, "")
+        self.assertEqual(user.last_name, "")
 
-    def test_email(self):
-        self.assertIsInstance(self.user.email, str)
-        self.assertEqual(self.user.email, "")
-
-    def test_password(self):
-        self.assertIsInstance(self.user.password, str)
-        self.assertEqual(self.user.password, "")
-
-    def test_user_pep8(self):
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/user.py'])
-        self.assertEqual(result.total_errors, 0, "PEP8 errors found")
-
-    def test_pep8(self):
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/test_user.py'])
-        self.assertEqual(result.total_errors, 0, "PEP8 errors found")
-
-
-if __name__ == "__main__":
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
