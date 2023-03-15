1cc9a1b8ed34b66a668fffdc1d91274253e303a6
emelyne1234 <105202770+emelyne1234@users.noreply.github.com>
2023-03-15 21:27:14 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 
 import uuid
 from datetime import datetime
+from time import sleep
 
 
 class BaseModel:
@@ -12,7 +13,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 key = value
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.utcnow())
+        self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
 
     def __str__(self) -> str:
@@ -22,12 +23,11 @@ class BaseModel:
     def save(self):
         """instance method to update time """
         self.updated_at = datetime.now()
-        return self.updated_at
 
     def to_dict(self):
         """instance method to return dictionary containing all keys/values"""
         the_dict = self.__dict__.copy()
         the_dict['__class__'] = self.__class__.__name__
-        the_dict['self.created_at'] = self.created_at.isoformat()
-        the_dict['self.updated_at'] = self.updated_at.isoformat()
+        the_dict['created_at'] = str(self.created_at.isoformat())
+        the_dict['updated_at'] = str(self.updated_at.isoformat())
         return the_dict
