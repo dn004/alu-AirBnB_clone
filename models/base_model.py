01859fe4bd5eefffdb6eca96080ad519ac53029d
emelyne1234 <105202770+emelyne1234@users.noreply.github.com>
2023-04-24 15:09:09 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,24 +8,24 @@ import models
 class BaseModel:
     """BaseClass that define al common Attributes/methods for other classes"""
 
-   def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         self.id = kwargs.get('id', str(uuid.uuid4()))
         self.updated_at = kwargs.get('updated_at', datetime.utcnow())
         self.created_at = kwargs.get('create_at', datetime.utcnow())
         models.storage.new(self)
 
 
-   def __str__(self) -> str:
+    def __str__(self) -> str:
         """a method to implement object"""
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
-   def save(self):
+    def save(self):
         """instance method to update time """
         self.updated_at = datetime.now()
         models.storage.save()
 
 
-   def to_dict(self):
+    def to_dict(self):
         """instance method to return dictionary containing all keys/values"""
         the_dict = self.__dict__.copy()
         the_dict['__class__'] = self.__class__.__name__
