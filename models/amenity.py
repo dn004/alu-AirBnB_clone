4b9d8e7297b6ddebfd046520717fecbc81e98b23
emelyne1234 <105202770+emelyne1234@users.noreply.github.com>
2023-04-24 15:45:45 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
-
-#Model
+# Model
 from models.base_model import BaseModel
+# class with attribute "name"
 
-#class with attribute "name"
 
 class Amenity(BaseModel):
     name = ""
